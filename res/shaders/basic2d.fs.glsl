3c1f18eb3f04e186d9ed0219f91703aa5a98dffe
Belos Thomas <belos.thomas@outlook.com>
2018-11-27 17:46:21 +0100

    basic2d

---
+++ res/shaders/basic2d.fs.glsl
@@ -0,0 +1,13 @@
+#version 300 es
+
+precision mediump float;
+
+in vec2 vPosition;
+in vec2 vNormal;
+in vec2 vTexCoord;
+
+out vec4 fFragColor;
+
+void main() {
+    fFragColor = vec4(1, 1, 1, 1);
+}
---
+++ res/shaders/basic2d.vs.glsl
@@ -0,0 +1,21 @@
+#version 300 es
+
+precision mediump float;
+
+layout(location = 0) in vec3 aVertexPosition;
+layout(location = 1) in vec3 aVertexNormal;
+layout(location = 2) in vec3 aVertexTexCoord;
+
+out vec2 vPosition;
+out vec2 vNormal;
+out vec2 vTexCoord;
+
+uniform mat4 uMVPMatrix;
+
+
+void main() {
+	vPosition = aVertexPosition.xy;
+	vNormal = aVertexNormal.xy;
+	vTexCoord = aVertexTexCoord.xy;
+    gl_Position = vec4(mat3(uMVPMatrix) * vec3(vPosition, 1), 1);
+}
